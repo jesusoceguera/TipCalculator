9072098407bad34dba547da260dd91ff09512bfd
JESUS OCEGUERA-PENA <5002167529@mwce111-18.local>
2020-01-23 19:46:13 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by JESUS OCEGUERA-PENA on 1/23/20.
+//  Copyright © 2020 JESUS OCEGUERA-PENA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
